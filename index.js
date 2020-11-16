a2b6779c64ce0a541ca755714ac13a33ae2693f8
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-11-16 15:29:27 +0100

    ..

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ fetch(baseUrl, {
                     avatarUrl
                     bio
                     name
-                    repositories(last: 20, orderBy: {field: NAME, direction: DESC}) {
+                    repositories(first: 20, orderBy: {field: CREATED_AT, direction: DESC}) {
                             pageInfo {
                                 hasNextPage
                                 endCursor
@@ -93,7 +93,7 @@ fetch(baseUrl, {
 })
 .then(res => res.json())
 .then(res => {
-    console.log(res.data.viewer.repositories)
+    console.log(res.data.viewer.repositories.nodes)
     let avatarUrl = res.data.viewer.avatarUrl
     let bio =  res.data.viewer.bio
     document.querySelector('.profile_name').textContent =  res.data.viewer.name
@@ -109,10 +109,12 @@ fetch(baseUrl, {
     })
     document.querySelector('.dp_img').src = avatarUrl
     let count = 0;
-
+    console.log(res.data.viewer.repositories.nodes.length)
+    let publicReposCount = res.data.viewer.repositories.nodes.filter(el => !el.isPrivate).length
+    document.querySelector('.publicReposCount').textContent = publicReposCount;
     res.data.viewer.repositories.nodes.forEach(el => {
         count++;
-        console.log(count);
+        console.log(el);
         let child = `
         <div class="repo_list_wrapper">
             <div class="repo_list">
@@ -127,15 +129,15 @@ fetch(baseUrl, {
                     <div class="content_info null">
                             <span class="repo_info_svg_parent">
                                 <svg class="octicon octicon-star mr-1 repo_info_svg" fill="#959da5" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
-                            </span><span class="tab_name_span"> ${el.stargazerCount}</span> 
+                            </span><span class="tab_name_span"> ${el.stargazerCount ? el.stargazerCount : null}</span> 
                     </div>
                     <div class="content_info null">
                             <span class="repo_info_svg_parent">
                                 <svg aria-label="fork" class="octicon octicon-repo-forked repo_info_svg" fill="#959da5" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>
-                            </span><span class="tab_name_span"> ${el.forkCount}</span>
+                            </span><span class="tab_name_span"> ${el.forkCount ? el.forkCount : null}</span>
                     </div>
                         <div class="content_info">
-                            <span>Updated  ${timeSince(new Date(el.updatedAt))}</span>
+                            <span>Updated ${el.updatedAt ? timeSince(new Date(el.updatedAt)) : null}</span>
                         </div>
                         
                     </div>
@@ -149,11 +151,10 @@ fetch(baseUrl, {
             </div>
         </div>
         `;
-        document.querySelector('.repo_list_container').insertAdjacentHTML('beforeend' , child)
+        document.querySelector('.repo_list_container').insertAdjacentHTML('afterend' , child)
     });
    
-    
 })
 .catch(err => {
-    console.log(JSON.stringify(err))
+    console.log(JSON.stringify(err.data))
 })
