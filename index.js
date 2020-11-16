f60d486f0ccb0fa213e057b788b87e9558daf807
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-11-16 06:01:06 +0100

    ..

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const github_data = {
-    "token": "e5db3875cf781acd309c54fd5fe20f22afc9862b",
-    "username": 'EBEN4REAL'
-}
-
-
 document.querySelector('.octicon-three-bars').addEventListener('click' , (e) => {
     if(document.querySelector('.search_wrapper').classList.contains('show-mobile-nav-list')) {
         document.querySelector('.search_wrapper').classList.remove('show-mobile-nav-list')
@@ -16,34 +10,119 @@ document.querySelector('.octicon-three-bars').addEventListener('click' , (e) => 
         document.querySelector('.nav_menu_wrapper').classList.add('show-mobile-nav-list')
     }
 })
-
-const body = {
-    "query": `
-        user(login: ${github_data["username"]}) {
-            issues(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
-                nodes {
-                    title,
-                    body,
-                    closedAt
-                }
-            }
-        }
-    `
+const github_data = {
+    "token": "01f1bc6b09dc3410ac647d288c9d6bdc1ae1f3a3",
+    "username": 'EBEN4REAL'
 }
-
 const baseUrl = "https://api.github.com/graphql";
 
 const headers = {
     "Content-Type" : "application/json",
-    Authentication: "bearer " + github_data["token"]
+    Authorization: "bearer " + "01f1bc6b09dc3410ac647d288c9d6bdc1ae1f3a3"
 }
+
+
 fetch(baseUrl, {
-    method: "GET",
+    method: "POST",
     headers: headers,
-    body: JSON.stringify(body)
-}).then(res => {
-    console.log(JSON.stringify(res))
-}).catch(err => {
+    body: JSON.stringify({
+        query: `
+            query {
+                viewer {
+                    avatarUrl
+                    bio
+                    name
+                    repositories(last: 20, orderBy: {field: NAME, direction: DESC}) {
+                            pageInfo {
+                                hasNextPage
+                                endCursor
+                            }
+                            nodes {
+                            name
+                            url
+                            updatedAt
+                            isPrivate
+                            owner {
+                                login
+                            }
+                            defaultBranchRef {
+                                name
+                            }
+                            primaryLanguage {
+                                name
+                                color
+                            }
+                            stargazerCount
+                            forkCount
+                            descriptionHTML
+                            description
+                        }
+                    }
+                }
+            } 
+             
+        `
+    })
+})
+.then(res => res.json())
+.then(res => {
+    console.log(res.data.viewer.repositories)
+    let avatarUrl = res.data.viewer.avatarUrl
+    let bio =  res.data.viewer.bio
+    document.querySelector('.profile_name').textContent =  res.data.viewer.name
+    Array.from(document.querySelectorAll('.name')).forEach(el => {
+        console.log(el)
+        el.innerHTML = res.data.viewer.name
+    })
+    Array.from(document.querySelectorAll('.bio')).forEach(el => {
+        el.textContent = bio
+    })
+    Array.from(document.querySelectorAll('.dp_img')).forEach(el => {
+        el.src = avatarUrl
+    })
+    document.querySelector('.dp_img').src = avatarUrl
+    res.data.viewer.repositories.nodes.forEach(el => {
+        let child = `
+        <div class="repo_list_wrapper">
+            <div class="repo_list">
+                <div class="repo_info">
+                    <a class="repo_name" href="${el.url}">
+                        ${el.name}
+                    </a>
+                    <div class="repo_info_content">
+                        <div class="content_info">
+                            <span class="repo-language-color" style="background: ${el.primaryLanguage.color}"></span><span> ${el.primaryLanguage.name}</span>
+                        </div>
+                    <div class="content_info null">
+                            <span class="repo_info_svg_parent">
+                                <svg class="octicon octicon-star mr-1 repo_info_svg" fill="#959da5" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
+                            </span><span class="tab_name_span"> ${el.stargazerCount}</span> 
+                    </div>
+                    <div class="content_info null">
+                            <span class="repo_info_svg_parent">
+                                <svg aria-label="fork" class="octicon octicon-repo-forked repo_info_svg" fill="#959da5" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>
+                            </span><span class="tab_name_span"> ${el.forkCount}</span>
+                    </div>
+                        <div class="content_info">
+                            <span>Updated on ${el.updatedAt}</span>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="">
+                    <button class="star_repo">
+                        <svg class="octicon octicon-star mr-1 " fill="#959da5" viewBox="0 0 16 16" style="margin-right: 5px" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
+                        Star
+                    </button>
+                </div>
+            </div>
+        </div>
+        `;
+        document.querySelector('.repo_list_container').insertAdjacentHTML('beforeend' , child)
+    });
+   
+    
+})
+.catch(err => {
     console.log(JSON.stringify(err))
 })
-
