a362b7691f85bdcdff7accc5559ecb3ae9239f16
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-11-24 22:22:58 +0100

    ..

--- constant.js
+++ constant.js
@@ -1,4 +1,3 @@
 //Encoded token
-const token = atob("NTVkMGRiMjc2ZjExYmFlZjAwMTQzMDQzMGRkMmIzYTY2ZjRlYjY5Mg==");
+const token = atob("NDkwNDVjZDEyNTQ4M2ExYzQ3YzAxZWJiOGNhZTMwNGM3Y2I5ZDU4NQ==");
 
-// const token = "55d0db276f11baef001430430dd2b3a66f4eb692"
