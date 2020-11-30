78ed8147b07ca47f6f8dd6dce3af5cd4cbbdb16f
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-11-30 16:19:59 +0100

    ..

--- index.js
+++ index.js
@@ -195,25 +195,23 @@ fetch(baseUrl, {
 .catch(err => {
 })
 
-
-
 Array.from(document.querySelectorAll('.dropbtn')).forEach(el => {
     el.addEventListener('click', (e) => {
         el.parentNode.children[1].classList.toggle('show')
     })
 })
-window.onclick = (event) => {
-    if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-          }
-    }
-}
+// window.onclick = (event) => {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 window.addEventListener("scroll", (e) => {
     if (window.scrollY >= 370) {
         document.querySelector('.repo_logo').style.visibility = 'visible';
